12651c361e1abe875b30014a1aa3d51cf11e0338
Thurol <113917315+Thurol@users.noreply.github.com>
2025-01-25 18:20:46 +0100

    Update version.tf

--- version.tf
+++ version.tf
@@ -3,16 +3,16 @@ terraform {
     path = "terraform.tfstate"
   }
 
-  #required_version = "~> 1.8.4"
+  required_version = "~> 1.6.3"
 
-  #required_providers {
-    #aws = {
-     # version = "~> 4.67.0"
-     # source  = "hashicorp/aws"
-    #}
-    #null = {
-    #  source  = "hashicorp/null"
-    #  version = "3.2.1"
-   # }
-# }
+  required_providers {
+    aws = {
+      version = "~> 4.67.0"
+      source  = "hashicorp/aws"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+ }
 }
